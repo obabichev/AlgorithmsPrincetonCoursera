93d9fd84b288827e598a67d8bac4eef1283ab6cb
obabichev <Aluckard.13>
2018-02-11 12:07:51 +0300

    Point

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -5,11 +5,9 @@ public class Permutation {
         int k = Integer.parseInt(args[0]);
 
         final RandomizedQueue<String> q = new RandomizedQueue<>();
-        final String line = StdIn.readLine();
 
-        final String[] split = line.split(" ");
-        for (String word : split) {
-            q.enqueue(word);
+        while (StdIn.isEmpty()) {
+            q.enqueue(StdIn.readString());
         }
 
         for (int i = 0; i < k; i++) {
